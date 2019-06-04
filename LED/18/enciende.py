e18db14c2b61ac0704dc3e3deebf80a0ea99e7cd
John DoeRenato <renmanzo@hotmail.com>
2019-06-04 19:02:23 +0100

    caca

--- LED/18/enciende.py
+++ LED/18/enciende.py
@@ -1,11 +1,11 @@
 #!usr/bin/env/ python
-# enciende.py
-# Importamos la libreria y le cambiamos el nombre a GPIO
+## enciende.py
+#Importamos la libreria y le cambiamos el nombre a GPIO
 import RPi.GPIO as GPIO
-# Establecemos el sistema de numeracion que queramos,
-# en este caso el sistema BCM
+#Establecemos el sistema de numeracion que queramos, 
+#en este caso el sistema BCM
 GPIO.setmode(GPIO.BCM)
-# Configuramos el pin GPIO 4 como una salida
-GPIO.setup(18, GPIO.OUT)
-# Encendemos el led
-GPIO.output(18, GPIO.HIGH)
+#Configuramos el pin GPIO 4 como una salida
+GPIO.setup(4, GPIO.OUT)
+#Encendemos el led
+GPIO.output(4, GPIO.HIGH)
